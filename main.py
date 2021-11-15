5765968a4e73700dde3bfcd69d5dbf0206108b71
hidema1 <hidema12345@gmail.com>
2021-11-15 22:13:38 +0900

    オセロばんの線引き完了

--- main.py
+++ main.py
@@ -25,21 +25,27 @@ board_place = board_right, board_bottom=width*0.8, height*0.8
 board_width = board_right-board_left
 board_height = board_bottom-board_top
 
-top_dot = [(board_top, i) for i in numpy.arange(board_left+board_width/8, board_right, board_width/8)]
-bottom_dot = [(board_bottom, i) for i in numpy.arange(board_left + width/8, board_right, board_width/8)]
+#縦線
+top_dot = [(i, width * 0.1) for i in numpy.arange(board_left+board_width/7, board_right + board_width/7, board_width/7)]
+bottom_dot = [(i, height * 0.9) for i in numpy.arange(board_left + board_width/7, board_right+ board_width/7, board_width/7)]
 
+#横線
+left_dot = [(width * 0.1, i) for i in numpy.arange(board_top+board_height/7, board_bottom + board_height/7, board_height/7)]
+right_dot = [(height * 0.9, i) for i in numpy.arange(board_top + board_height/7, board_bottom+ board_height/7, board_height/7)]
 
+print(right_dot)
 
 def draw_lines(color, place, v1,v2):
-    for i in range(v1):
+    for i in range(0, len(v1)):
         pygame.draw.aaline(place, color,v1[i], v2[i])
-
 
 def board():
     #四角を表示
     pygame.draw.rect(screen, green, pygame.Rect(board_size,board_place))
     #top/leftからスタート
-    pygame.draw.aaline(screen, black, [100,162.5], [100,225.0], True)
+    pygame.draw.aaline(screen, black, top_dot[0], bottom_dot[0], True)
+    draw_lines(black, screen, top_dot, bottom_dot)
+    draw_lines(black, screen, left_dot, right_dot)
     
 
 #title
